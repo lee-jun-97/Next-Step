1edea1c6d2b6df8dcf359a2b1a78efd8ae59523d
LeeJun <yuog12@naver.com>
2022-03-15 22:57:17 +0900

    [ 2022 .03. 15 ] 1. 테스트 케이스 추가 2. 문자열 파싱 중 String API 학습 필요성 느껴 String API 학습 하여 정리 예정

--- src/main/java/com/next/strcal/StringCalculator.java
+++ src/main/java/com/next/strcal/StringCalculator.java
@@ -1,12 +1,33 @@
 package com.next.strcal;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class StringCalculator {
 	
-	public int sum(String str) {
+	public int split(String str) {
 		
 		int result= 0;
-		
+
+		String s;
+
+		s = str.trim();
+
+		if (s.contains("\n") == true ) {
+
+			s.split("\n");
+
+		} else {
+
+		}
+
 		return result ;
 	}
 
+	public int sum(String str) {
+
+		return 0;
+	}
+	
 }
--- src/test/java/com/next/strcal/StringCalculatorTest.java
+++ src/test/java/com/next/strcal/StringCalculatorTest.java
@@ -16,28 +16,40 @@ public class StringCalculatorTest {
 		// given
 		str = "1,2";
 		// when , then
-		assertEquals(3, strcal.sum(str));
+		assertEquals(3, strcal.split(str));
 		
 		str = "1:7:3";
-		assertEquals(11, strcal.sum(str));
+		assertEquals(11, strcal.split(str));
 		
 		// case 2
 		// given
 		str = "//%\n1%7%8";
 		// when , then
-		assertEquals(16, strcal.sum(str));
-		
+		assertEquals(16, strcal.split(str));
+
 		// case 3
 		// given
-		str = "-1:6";
-		// when, then
-		assertEquals(new RuntimeException(), strcal.sum(str));
+		str = "$#//^\n4^7^3";
+		assertEquals(14, strcal.split(str));
 		
 		// case 4
 		// given
+		str = "-1:6";
+		// when, then
+		assertEquals(new RuntimeException(), strcal.split(str));
+		
+		// case 5
+		// given
 		str = "//%\n1%-7%8";
 		// when , then
-		assertEquals(new RuntimeException(), strcal.sum(str));
+		assertEquals(new RuntimeException(), strcal.split(str));
+
+		// case 6
+		// given
+		str = "    //%\n1%-7%8";
+		// when , then
+		assertEquals(new RuntimeException(), strcal.split(str));
+
 		
 	}
 	
