37ba449edb22c537ad6367a777188cc756deaf90
LeeJun <yuog12@naver.com>
2022-03-19 23:40:49 +0900

    [ 2022 .03. 19 ] 1. Test Case 4 의 연산자를 ^ 로 지정하였을 시 split method 가 정상 동작이 되지 않는 현상 있음.

--- src/main/java/com/next/strcal/StringCalculator.java
+++ src/main/java/com/next/strcal/StringCalculator.java
@@ -1,33 +1,47 @@
 package com.next.strcal;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 	
-	public int split(String str) {
-
-		String[] strList = {};
-
-		if ( str.contains(",") == true) {
-			strList = str.split(",");
-		} else if ( str.contains(":") == true ) {
-			strList = str.split(":");
+	public int cal(String str) {
+		
+		if(str.isEmpty()) {
+			return 0;
 		}
+		
+		String[] strList = str.split(",|:");
+
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(str.trim());
+		if ( m.find()) {
+			String sep = m.group(1);
+			strList = m.group(2).split(sep);
+		}
+
+		numCheck(strList);
 		
 		return sum(strList);
 	}
 
-	public int sum(String[] str) {
+	public int sum(String[] strList) {
 
 		int sum = 0;
 
-		for ( String i : str ) {
+		for ( String i : strList ) {
 			sum += Integer.parseInt(i) ;
 		}
 
 		return sum;
 	}
-	
+
+	public void numCheck(String[] strList) {
+
+		for ( String i : strList ) {
+			if ( Integer.parseInt(i) < 0 ) {
+				throw new RuntimeException();
+			}
+		}
+	}
+
 }
--- src/test/java/com/next/strcal/StringCalculatorTest.java
+++ src/test/java/com/next/strcal/StringCalculatorTest.java
@@ -1,6 +1,7 @@
 package com.next.strcal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,42 +17,52 @@ public class StringCalculatorTest {
 		// given
 		str = "1,2";
 		// when , then
-		assertEquals(3, strcal.split(str));
+		assertEquals(3, strcal.cal(str));
 		
 		// case 2
 		// given
 		str = "1:7:3";
 		//when, then
-		assertEquals(11, strcal.split(str));
+		assertEquals(11, strcal.cal(str));
 		
 		// case 3
 		// given
 		str = "//%\n1%7%8";
 		// when , then
-		assertEquals(16, strcal.split(str));
+		assertEquals(16, strcal.cal(str));
 
 		// case 4
 		// given
-		str = "$#//^\n4^7^3";
-		assertEquals(14, strcal.split(str));
+		// str = "//^\n4^7^3";
+		str = "//%\n1%7%8";
+		assertEquals(16, strcal.cal(str));
 		
 		// case 5
 		// given
-		str = "-1:6";
+		// "-1:6"
 		// when, then
-		assertEquals(new RuntimeException(), strcal.split(str));
+		assertThrows(RuntimeException.class, () -> {
+			String ex = "-1:6";
+			strcal.cal(ex);
+		});
 		
 		// case 6
 		// given
-		str = "//%\n1%-7%8";
+		// str = "//%\n1%-7%8";
 		// when , then
-		assertEquals(new RuntimeException(), strcal.split(str));
+		assertThrows(RuntimeException.class, () -> {
+			String ex = "//%\n1%-7%8";
+			strcal.cal(ex);
+		});
 
 		// case 7
 		// given
-		str = "    //%\n1%-7%8";
+		// str = "    //%\n1%-7%8";
 		// when , then
-		assertEquals(new RuntimeException(), strcal.split(str));
+		assertThrows(RuntimeException.class, () -> {
+			String ex = "    //%\n1%-7%8";
+			strcal.cal(ex);
+		});
 
 		
 	}
